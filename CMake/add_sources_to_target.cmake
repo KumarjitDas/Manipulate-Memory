55d96eec9106c74d813000e95bca944189cf657b
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-12-07 17:24:35 +0530

    Added private utility macros
    Adding `_KDI_ARCHITECTURE_64_BIT` macro definition.
    Adding `_KDI_MEMORY_COPY_SIZE_FOR_WHEN_TO_ALIGN` macro definition.

--- CMake/add_sources_to_target.cmake
+++ CMake/add_sources_to_target.cmake
@@ -3,7 +3,7 @@
 # author: Kumarjit Das (kumarjitdas1999@gmail.com)
 # brief: Add the C source files to the project library.
 # version: 0.1.0
-# date: 2021-12-05
+# date: 2021-12-07
 #
 # License(MIT License):
 #
@@ -41,7 +41,11 @@ target_sources(${KDI_LIBRARY_NAME} PRIVATE ${INCLUDE_FILE})
 
 # Adding the source files to the main target
 set(SRC_FILES "${SRC_DIRECTORY}/manipulate_memory/manipulate_memory.c"
-              "${SRC_DIRECTORY}/manipulate_memory/utility.h")
+              "${SRC_DIRECTORY}/manipulate_memory/utility.h"
+              "${SRC_DIRECTORY}/manipulate_memory/alignment.h"
+              "${SRC_DIRECTORY}/manipulate_memory/alignment.c"
+              "${SRC_DIRECTORY}/manipulate_memory/copy.h"
+              "${SRC_DIRECTORY}/manipulate_memory/copy.c")
 target_sources(${KDI_LIBRARY_NAME} PRIVATE ${SRC_FILES})
 
 # Setting the install destination for include and source file
