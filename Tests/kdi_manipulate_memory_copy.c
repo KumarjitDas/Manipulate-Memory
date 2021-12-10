622d55a90f5b64c253a0d3d9acf444eb834738c6
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-12-10 16:13:52 +0530

    Removed old documentation and added doxygen documentation

--- Tests/kdi_manipulate_memory_copy.c
+++ Tests/kdi_manipulate_memory_copy.c
@@ -1,11 +1,11 @@
 /**
- * @file kdi_manipulate_memory_copy.c
- * @author Kumarjit Das (kumarjitdas1999@gmail.com)
- * @brief Tests for `kdi_manipulate_memory_copy` function.
- * @version 0.1.0
- * @date 2021-12-07
+ * \file kdi_manipulate_memory_copy.c
+ * \author Kumarjit Das (kumarjitdas1999@gmail.com)
+ * \brief Tests for `kdi_manipulate_memory_copy` function.
+ * \version 0.1.0
+ * \date 2021-12-10
  *
- * @copyright Copyright (c) 2021
+ * \copyright Copyright (c) 2021
  *
  * License(MIT License):
  *
