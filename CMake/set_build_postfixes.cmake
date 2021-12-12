6b770f63cb47d76156f454eb21bc8389a20dadfe
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2021-12-12 16:24:39 +0530

    Added build postfixes to output files
    Changing `STATIC_POSTFIX` to `KDI_STATIC_POSTFIX`
    Adding build postfixes for example and test executables

--- CMake/set_build_postfixes.cmake
+++ CMake/set_build_postfixes.cmake
@@ -32,15 +32,15 @@ write_status("Setting build postfixes.")
 
 # Add `-s` as postfix to the build output when building static libraries
 if(NOT BUILD_SHARED_LIBS)
-    set(STATIC_POSTFIX "-s")
+    set(KDI_STATIC_POSTFIX "-s")
 endif()
 
 # Setting build postfixes for different build configurations
 set_target_properties(
     ${KDI_LIBRARY_NAME}      PROPERTIES
     OUTPUT_NAME            "${KDI_LIBRARY_NAME}"
-    DEBUG_POSTFIX          "${STATIC_POSTFIX}-d"
-    RELEASE_POSTFIX        "${STATIC_POSTFIX}"
-    MINSIZEREL_POSTFIX     "${STATIC_POSTFIX}-mr"
-    RELWITHDEBINFO_POSTFIX "${STATIC_POSTFIX}-rd"
+    DEBUG_POSTFIX          "${KDI_STATIC_POSTFIX}-d"
+    RELEASE_POSTFIX        "${KDI_STATIC_POSTFIX}"
+    MINSIZEREL_POSTFIX     "${KDI_STATIC_POSTFIX}-mr"
+    RELWITHDEBINFO_POSTFIX "${KDI_STATIC_POSTFIX}-rd"
 )
